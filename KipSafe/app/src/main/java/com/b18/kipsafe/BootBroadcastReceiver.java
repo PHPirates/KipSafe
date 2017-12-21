66f6e750ce4f0aee0cf727fa6e8c0050226960e9
Thomas <hollandpirates@gmail.com>
2017-12-21 21:11:13 +0100

    App now checks if alarm should be set on reboot

--- KipSafe/app/src/main/java/com/b18/kipsafe/BootBroadcastReceiver.java
+++ KipSafe/app/src/main/java/com/b18/kipsafe/BootBroadcastReceiver.java
@@ -19,22 +19,28 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context pContext, Intent intent) {
         KipAlarmManager alarmManager = new KipAlarmManager(pContext);
         SharedPreferenceManager preferenceManager = new SharedPreferenceManager(pContext);
-        Calendar time;
-        try {
-            time = preferenceManager.getAlarmTime();
-            // debug
-//        time = Calendar.getInstance();
-//        time.set(Calendar.SECOND, time.get(Calendar.SECOND) + 10);
-            Calendar now = Calendar.getInstance();
-            if (now.compareTo(time) > 0) {
-                // Now is after the sunset of today
-                // So schedule alarm for next sunset
-                time.set(Calendar.DAY_OF_MONTH, time.get(Calendar.DAY_OF_MONTH) + 1);
+
+        if (preferenceManager.getIsAlarmSet()) {
+
+            // If the alarm was scheduled, reschedule it with the last time known. If no last time
+            // is known, request new sunset time.
+            Calendar time;
+            try {
+                time = preferenceManager.getAlarmTime();
+                // debug
+//                time = Calendar.getInstance();
+//                time.set(Calendar.SECOND, time.get(Calendar.SECOND) + 10);
+                Calendar now = Calendar.getInstance();
+                if (now.compareTo(time) > 0) {
+                    // Now is after the sunset of today
+                    // So schedule alarm for next sunset
+                    time.set(Calendar.DAY_OF_MONTH, time.get(Calendar.DAY_OF_MONTH) + 1);
+                }
+                alarmManager.setAlarm(time);
+            } catch (DataNotFoundException e) {
+                int minutesBeforeSunset = preferenceManager.getPrefTime();
+                new AlarmSetter(pContext).set(minutesBeforeSunset, GetSunSetTask.Delay.NO_DELAY);
             }
-            alarmManager.setAlarm(time);
-        } catch (DataNotFoundException e) {
-            int minutesBeforeSunset = preferenceManager.getPrefTime();
-            new AlarmSetter(pContext).set(minutesBeforeSunset, GetSunSetTask.Delay.NO_DELAY);
         }
     }
 }
