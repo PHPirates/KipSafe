b503f5f528c3fd2ec4b84869778397003b4b21b1
slideclimb <abbyberkers@gmail.com>
2018-02-01 13:03:02 +0100

    refactored BootBroadcastReceiver

---
+++ app/src/main/java/com/b18/kipsafe/BootBroadcastReceiver.kt
@@ -0,0 +1,37 @@
+package com.b18.kipsafe
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.b18.kipsafe.SunsetCommunication.GetSunSetTask
+import com.b18.kipsafe.alarms.KipAlarmManager
+import java.util.*
+
+/**
+ * Reschedule alarm after reboot.
+ */
+class BootBroadcastReceiver : BroadcastReceiver(){
+    override fun onReceive(pContext: Context?, intent: Intent?) {
+        val alarmManager = KipAlarmManager(pContext)
+        val preferenceManager = SharedPreferenceManager(pContext)
+
+        if (preferenceManager.isAlarmSet) {
+            // If the alarm was scheduled, reschedule it with the last time known.
+            // If no time is known, request new sunset time.
+            val time: Calendar
+            try {
+                time = preferenceManager.alarmTime
+
+                val now = Calendar.getInstance()
+                if(now > time) {
+                    // Now is past today's sunset, so schedule alarm for next sunset.
+                    time.set(Calendar.DAY_OF_MONTH, time.get(Calendar.DAY_OF_MONTH) + 1)
+                }
+                alarmManager.setAlarm(time)
+            } catch (e: DataNotFoundException){
+                AlarmSetter(pContext).set(preferenceManager.prefTime,
+                        GetSunSetTask.Delay.NO_DELAY)
+            }
+        }
+    }
+}
